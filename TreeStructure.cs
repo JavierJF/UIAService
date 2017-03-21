08d800bda1e186034d7d48493a8338aa44cd3454
Javier Jaramago Fernández <jaramago.fernandez.javier@gmail.com>
2017-03-21 15:54:53 +0100

    Initial service structure

---
+++ TreeStructure.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestUIAut
+{
+    public class NBTree<T>
+    {
+        public NBTree<T> parent { get; set; }
+        public List<NBTree<T>> children { get; set; }
+
+        public NBTree()
+        {}
+
+        public NBTree(NBTree<T> parent, List<NBTree<T>> children)
+        {
+            this.parent = parent;
+            this.children = children;
+
+            foreach(var child in children)
+            {
+                child.parent = this;
+            }
+        }
+
+        public bool IsRoot { get { return parent == null; } }
+        public bool IsLeaf { get { return children.Count==0; } }
+    }
+}
