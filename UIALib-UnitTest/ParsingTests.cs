35f7bd7465895e7a878fee282de9cf8a54d74b6f
Javier Jaramago Fernández <jaramago.fernandez.javier@gmail.com>
2017-03-30 20:24:24 +0200

    Added new project for TDD

---
+++ UIALib-UnitTest/ParsingTests.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using LanguageExt;
+using static LanguageExt.Prelude;
+
+namespace UIALib_UnitTest
+{
+    [TestFixture]
+    public class ParsingTests
+    {
+    }
+}
