f1f7d3bf5d2ca49d960158698a701a046f4d42df
Javier Jaramago Fernández <jaramago.fernandez.javier@gmail.com>
2017-04-03 13:02:10 +0200

    LICENSE file and headers

--- UIALib/UIABinds/TraversingFns.cs
+++ UIALib/UIABinds/TraversingFns.cs
@@ -1,4 +1,12 @@
-﻿using System;
+﻿/*
+ * The R&D leading to these results received funding from the
+ * Department of Education - Grant H421A150005 (GPII-APCP). However,
+ * these results do not necessarily represent the policy of the
+ * Department of Education, and you should not assume endorsement by the
+ * Federal Government.
+ */
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
