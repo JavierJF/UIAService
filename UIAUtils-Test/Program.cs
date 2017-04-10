e05fe3c171e2243a08eb27bd26d729247cbd7b51
Javier Jaramago Fernández <Javier Jaramago Fernndez>
2017-04-10 17:43:48 +0200

    Added correct Testing project

--- UIAUtils-Test/Program.cs
+++ UIAUtils-Test/Program.cs
@@ -4,57 +4,95 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Automation;
-using LanguageExt;
-using static LanguageExt.Prelude;
+using UIALib.Components;
+using UIALib.Types;
 using UIALib;
+using LanguageExt;
 
 namespace UIAUtils_Tes
 {
     class Program
     {
-        public static List<VTreeNode> logPath =
-            new List<VTreeNode>
-            {
-                { new VTreeNode {name = "Word"} }
-                , { new VTreeNode {name = "MsoDockTop"} }
-                , { new VTreeNode {name = "&Menú"} }
-                , { new VTreeNode {name = "Menú"} }
-                , { new VTreeNode {name = "panel"} }
-                , { new VTreeNode {name = "Panel"} }
-            };
-
-        public static readonly List<Either<STreeNode, CTreeNode>> testPath =
-            new List<Either<STreeNode, CTreeNode>> {
-                new STreeNode { name = "Menú Lectura de pantalla completa"
-                              , nextMove = Move.Child }
-                , new STreeNode { name = "Pestaña Archivo"
-                              , nextMove = Move.Sibling }
-                , new STreeNode { name = "Herramientas"
-                              , nextMove = Move.Sibling }
-                , new CTreeNode { sNode = new STreeNode { name = "Vista", nextMove = Move.Child }
-                                , action = NodeAction.Expand
-                                , path = logPath
-                                , postActionEvents =
-                                    new Tuple<AutomationEvent, StructureChange>(
-                                        AutomationElementIdentifiers.MenuOpenedEvent
-                                        , new StructureChange { changeType = StructureChangeType.ChildAdded
-                                                              , scope = TreeScope.Children }
-                                    )}
-                , new STreeNode { name = ""
-                                , nextMove = Move.Child}
-                , new STreeNode { name = "Menú Grupo"
-                                , nextMove = Move.Child}
-                , new STreeNode { name = ""
-                                , nextMove = Move.Sibling }
-                , new STreeNode { name = ""
-                                , nextMove = Move.Sibling }
-                , new STreeNode { name = ""
-                                , nextMove = Move.Child }
-            };
+        static List<EWComp<object, object>> ewComps =
+            new List<EWComp<object, object>>
+                {
+                    new UIANode("Microsoft-Word"
+                               , "Window-App"
+                               , new Dictionary<string, string>
+                                    { {"Backend", "UI-Automation"} }
+                               , new List<string>
+                                    { "Node-Creation" }
+                               , new List<Either<STreeNode,CTreeNode>>
+                                   {
+                                       new STreeNode
+                                       {
+                                           name = "MsoDockTop"
+                                           , nextMove = Move.Child
+                                       },
+                                       new STreeNode
+                                       {
+                                           name = ""
+                                           , nextMove = Move.Sibling
+                                       },
+                                       new STreeNode
+                                       {
+                                           name = ""
+                                           , nextMove = Move.Child
+                                       },
+                                       new STreeNode
+                                       {
+                                           name = "Menú Lectura de pantalla completa"
+                                           , nextMove = Move.Child
+                                       },
+                                       new STreeNode
+                                       {
+                                           name = ""
+                                           , nextMove = Move.Child
+                                       },
+                                       new STreeNode
+                                       {
+                                           name = ""
+                                           , nextMove = Move.Child
+                                       },
+                                       new STreeNode {
+                                           name = "Menú Lectura de pantalla completa"
+                                           , nextMove = Move.Child
+                                       },
+                                       new STreeNode {
+                                           name = "Pestaña Archivo"
+                                           , nextMove = Move.Sibling
+                                       }, new STreeNode {
+                                           name = "Herramientas"
+                                           , nextMove = Move.Sibling
+                                       }
+                                   }
+                               )
+                };
+        static List<EComp> eComps =
+            new List<EComp>
+                {
+                    new AppWindowDetector()
+                };
+        static List<WComp<object>> wComps =
+            new List<WComp<object>>
+                {
+                    new Logger()
+                };
 
         static void Main(string[] args)
         {
-            TF.manualTreeTrasversing(testPath);
+            foreach(var wcomp in wComps)
+            {
+                foreach(var ecomp in eComps)
+                {
+                    var res = wcomp.watchedComps.Find((s) => s == ecomp.name);
+                    if (res != null)
+                    {
+                        ecomp.Subscribe(wcomp);
+                    }
+                }
+            }
+            Console.ReadLine();
         }
     }
 }
